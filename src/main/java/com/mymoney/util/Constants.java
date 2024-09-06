3de002b40ba2739afe7787ea8e7eac33d45503f3
Lucas Araújo <araujolucas@dcc.ufmg.br>
2024-09-06 19:35:32 -0300

    Remover constantes inutilizadas

--- src/main/java/com/mymoney/util/Constants.java
+++ src/main/java/com/mymoney/util/Constants.java
@@ -11,9 +11,6 @@ package com.mymoney.util;
  */
 public final class Constants
 {
-    public static final String ENTITY_MANAGER_PRODUCTION = "my_money_production";
-    public static final String ENTITY_MANAGER_TEST       = "my_money_test";
-    public static final String DB_TEST_FILE              = "data/test.db";
     public static final String LOG_FILE                  = "log/mymoney.log";
     public static final Double EPSILON                   = 1e-6;
     public static final int    MAX_BILLING_DUE_DAY       = 28;
