a8610a0941819cfd622958beca8fb4433149e104
luk3rr <luk3rr@gmail.com>
2024-10-07 01:07:18 -0300

    Atualizar imagem

--- src/test/resources/dummy-data.sql
+++ src/test/resources/dummy-data.sql
@@ -101,14 +101,15 @@ INSERT INTO wallet_transaction (id, wallet_id, category_id, type, status, date, 
 
 INSERT INTO credit_card (id, operator_id, name, billing_due_day, max_debt, last_four_digits) VALUES
 (1, 1, 'Visa Gold', 10, 5000.00, '1234'),
-(2, 2, 'MasterCard Platinum', 15, 7500.00, '5678'),
+(2, 2, 'MC Platinum', 15, 7500.00, '5678'),
 (3, 3, 'Amex Green', 20, 3000.00, '9101'),
 (4, 4, 'Discover Cashback', 25, 2000.00, '1121'),
 (5, 5, 'Diners Club Rewards', 30, 4000.00, '3141'),
 (6, 6, 'JCB', 10, 1000.00, '3830'),
 (7, 7, 'Elo', 11, 2300.00, '4301'),
 (8, 8, 'Hipercard', 5, 500.00, '9031'),
-(9, 0, 'Cartaozin', 5, 1.00, '1683');
+(9, 0, 'Cartaozin', 5, 1.00, '1683'),
+(10, 0, 'Cartaozin2', 5, 2.00, '3031');
 
 -- Dívidas passadas (já pagas)
 INSERT INTO credit_card_debt (id, crc_id, category_id, date, total_amount, description)
